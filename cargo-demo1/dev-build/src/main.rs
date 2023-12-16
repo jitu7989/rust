708982edef0dfac3847693991e3af8d7157a7f16
jitendra791 <jitendra@platformcommons.com>
2023-12-16 19:42:10 +0530

    test commit

---
+++ cargo-demo1/dev-build/src/main.rs
@@ -0,0 +1,12 @@
+/// Adds one to the number give
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+/// assert_eq!(6,answer);
+/// ```
+fn main() {
+    println!("Hello, world!");
+}
